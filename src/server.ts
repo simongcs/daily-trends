2a7b7102ac22f66a9dafb30c2c20624b86fadbd8
simongcs <12417560+simongcs@users.noreply.github.com>
2024-10-02 13:52:38 +0200

    Fix path

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import config from './config/config';
-import logger from './config/logger';
+import logger from './utils/logger';
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);

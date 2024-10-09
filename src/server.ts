cd4691978f4cf27fa5d7464e0a47bc44e912a40d
simongcs <12417560+simongcs@users.noreply.github.com>
2024-10-09 22:32:50 +0200

    Update server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,29 @@
+import { FeedModel } from './models/feed.model';
 import app from './app';
 import config from './config/config';
+import FeedRepository from './repositories/feed.repository';
+import FeedService from './services/feed.service';
+import ScrapingService from './services/scrapingFeed.service';
 import logger from './utils/logger';
+import feedUrls from './config/feeds';
+import Bootstrapper from './utils/bootstraper';
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
-});
+const feedRepository = new FeedRepository(FeedModel);
+const scrapingService = new ScrapingService(feedUrls.map(feed => feed.url));
+const feedService = new FeedService(feedRepository);
+const bootstrapper = new Bootstrapper(scrapingService, feedService);
+
+const startServer = async () => {
+  try {
+    await bootstrapper.runOnStartup();
+
+    app.listen(config.PORT, () => {
+      logger.info(`Server running on port ${config.PORT}`);
+    });
+  } catch (error) {
+    logger.error('Error during startup process:', error);
+    process.exit(1); // Exit the process if startup fails
+  }
+};
+
+startServer();

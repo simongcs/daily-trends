9b2286fcc2bb4a525eac8d7db760293be174f8d4
simongcs <12417560+simongcs@users.noreply.github.com>
2024-10-09 22:32:15 +0200

    Add db to app.test

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,13 +1,22 @@
 import request from 'supertest';
 import app from '../src/app';
+import Database from '../src/config/db';
 
 jest.mock('../src/utils/logger.ts', () => ({
   info: jest.fn(),
+  error: jest.fn(),
 }));
 
+jest.mock('../src/config/db.ts');
+
 describe('app tests', () => {
+  let db: Database;
+  beforeEach(() => {
+    db = new Database();
+  });
   describe('GET /health', () => {
     it('should return 200', async () => {
+      db.connect = jest.fn().mockReturnValue(true);
       const response = await request(app).get('/health');
       expect(response.status).toBe(200);
     });
